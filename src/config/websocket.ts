65bc91282d0dfef183003f9bdce2709bd3a61807
ezemba chiamaka <ezembachiamaka@gmail.com>
2024-08-19 16:28:07 +0100

    socket connection changed

--- src/config/websocket.ts
+++ src/config/websocket.ts
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:8000");
+// export const socket = io("http://localhost:8000");
+export const socket = io("https://server.thestreamlink.com");
 
 socket.on("connect", () => {
   console.log(socket.id); // x8WIv7-mJelg7on_ALbx
