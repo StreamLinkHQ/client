9b776afba79f69feeb07d90d47978f0303f0d518
ezemba chiamaka <ezembachiamaka@gmail.com>
2024-08-24 16:27:13 +0100

    logo added

--- src/features/ui/base/footer.tsx
+++ src/features/ui/base/footer.tsx
@@ -2,7 +2,7 @@ import { BsTwitterX, BsInstagram } from "react-icons/bs";
 const Footer = () => {
   return (
     <div className="w-full p-5 fixed bottom-0 bg-yellow text-black flex flex-row items-center justify-between">
-      <p>Logo</p>
+        <img src={"/logo.png"} alt="logo" className="w-[50px] lg:w-[280px]"/>
       <div className="flex flex-row items-center">
         <BsTwitterX className="text-2xl mr-8" />
 
--- src/features/ui/base/navbar.tsx
+++ src/features/ui/base/navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     <div className="bg-yellow flex flex-row justify-between px-6 py-3.5 items-center w-full mx-auto sticky top-0 box-border">
       <Link to="/">
         <div className="flex flex-row items-center lg:w-[15%] justify-between w-[30%]">
-          <p>Logo</p>
+        <img src={"/logo.png"} alt="logo" className="w-[150px] lg:w-[280px]"/>
         </div>
       </Link>
 
